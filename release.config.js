c1d81bfbbb3a33c139f402a91955dedac1e1bfab
Matthew Murray <matthew.murray@axiell.com>
2021-12-02 16:52:46 +0000

    updated release branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: "master",
+    branches: "main",
     repositoryUrl: "https://github.com/matthewtmurray/chop-ui",
     plugins: [
         "@semantic-release/commit-analyzer",
